08936602bfd840f134c46bba39f6b729b330e063
Sebastián Moreno Acero <sysma1997@gmail.com>
2021-09-17 17:36:28 -0500

    Init dates to index

---
+++ src/core/user/domain/user.js
@@ -0,0 +1,11 @@
+export class User {
+    constructor(id, 
+        name, 
+        email, 
+        phone) {
+        this.id = id
+        this.name = name
+        this.email = email
+        this.phone = phone
+    }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import "./index.css"
 
 const App = () => {
-    return <div>
-        <h1>success</h1>
-    </div>
+    return <>
+        <div className="header">
+            <p className="is-size-1">Welcome!</p>
+            <p className="is-size-4">
+                This is a simple example how to configure webpack with react and multiple pages.
+            </p>
+        </div>
+        <div className="content">
+            
+        </div>
+        <footer>
+            <p>
+                Created by <b>SYSMA</b>
+            </p>
+        </footer>
+    </>
 }
 
 const root = document.getElementById("root")
