1b6151562d10c8111f75f532efdeb5590939517c
Sebastián Moreno Acero <sysma1997@gmail.com>
2022-03-02 16:08:33 -0500

    Try add PDO to docker

---
+++ backend/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$mysqlHost = getenv("MYSQL_HOST");
+$mysqlDB = getenv("MYSQL_DB");
+$mysqlUser = getenv("MYSQL_USER");
+$mysqlPassword = getenv("MYSQL_PASSWORD");
+
+try {
+    $mdb = new PDO("mysql:host=$mysqlHost;dbname=$mysqlDB", $mysqlUser, $mysqlPassword);
+} catch (PDOException $pdoex) {
+    echo $pdoex->getMessage() . "<br />";
+    die();
+}
