63542c47f4c64918ca823232a7bfa68fb0ae5762
Sebastián Moreno Acero <sysma1997@gmail.com>
2022-03-25 17:23:56 -0500

    Add example to execute query in pdo mysql

--- backend/index.php
+++ backend/index.php
@@ -7,6 +7,10 @@ $mysqlPassword = getenv("MYSQL_PASSWORD");
 
 try {
     $mdb = new PDO("mysql:host=$mysqlHost;dbname=$mysqlDB", $mysqlUser, $mysqlPassword);
+    foreach($mdb->query("SELECT * FROM Users") as $row) {
+        print_r($row);
+    }
+    $mdb = null;
 } catch (PDOException $pdoex) {
     die($pdoex->getMessage() . "<br />");
 }
